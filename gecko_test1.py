c74c2f85ebc0159d4d931cc174a5a35601154221
Kit Haywood <kit.haywood@BTDOMAIN.LOCAL>
2021-10-12 16:41:56 +0100

    fully working version

--- gecko_test1.py
+++ gecko_test1.py
@@ -7,12 +7,14 @@ import json
 import tqdm
 import os
 from utils import date_range_lister
+
 # TODO - Go back and use market_chart_range to get 
  
 class Gecko:
     
     def __init__(self) -> None:
         self.cg = CoinGeckoAPI()
+
 
     def get_all_coins(self):
         return self.cg.get_coins_list()
@@ -40,12 +42,15 @@ class Gecko:
             json.dump(data.to_json(orient='split',date_format='iso'),f)
     
     def get_dates(self,crypto):
+        """
+        takes crypto - returns tuple of date extremities
+        """
         with open(os.path.join(os.getcwd(),f'{crypto}.json'),'r') as f:
             data = json.load(f)
-            data = json.loads(data)
-            res = pd.DataFrame.from_dict(data,orient='columns')
-            res.index = res['index']
-            return res.index.max(), res.index.min()
+        data = json.loads(data)
+        res = pd.DataFrame.from_dict(data,orient='columns')
+        res.index = res['index']
+        return res.index.max(), res.index.min()
     
     def get_data_on_interval(self,symbol,currency,from_date,to_date):
         """
@@ -56,6 +61,7 @@ class Gecko:
         base_url = f"https://api.coingecko.com/api/v3/coins/{symbol}/market_chart/range?vs_currency={currency}&from={from_date}&to={to_date}"
         response = requests.get(base_url)
         res = response.json()
+        print(res)
         data = res['prices']
         data = [[dt.datetime.fromtimestamp(x[0]/1000),x[1]] for x in data]
         df = pd.DataFrame(data)
@@ -64,71 +70,39 @@ class Gecko:
     def write_to_json(self,crypto,currency):
         # NOT WORKING YET - JUST SCRIBBLING
         dates = date_range_lister()
+        tracker = {}
+        tracker[crypto] = {}
         res = pd.DataFrame()
         for i in tqdm.tqdm(range(len(dates))):
-            if i==0:
+            if i==0: # careful
                 pass
             else:
-                data = self.get_data_on_interval(crypto,currency,dates[i],dates[i-1])
-                res = pd.concat([res,data])    
-            time.sleep(0.75)
-        
+                try:
+                    data = self.get_data_on_interval(crypto,currency,dates[i-1],dates[i])
+                    res = pd.concat([res,data])
+                except json.decoder.JSONDecodeError:
+                    print(dates[i],dates[i-1])
+                    tracker[crypto][dates[i]]=[dates[i],dates[i-1]]
+            time.sleep(1.2)
         return res
-
+    
+    def load_from_json(self,crypto):
+        with open(f"{crypto}.json",'r') as f_in:
+            data = json.loads(json.load(f_in))
+        return pd.DataFrame.from_dict(data['data'])
 
 if __name__=="__main__":
-
-    cryptos = ['bitcoin','ethereum','tether','cardarno','usd-coin']
-
-
-
-    # ETH = Gecko().get_market_data('ethereum','usd','max')
-    # print(ETH)
+    cryptos = ['cardano','usd-coin'] # Change this to the IDs of what you want to retrieve
 
     gck = Gecko()
-    # gck.write_original_to_json('ethereum','usd','1')
-    # print(gck.get_dates('ethereum'))
-    print(gck.write_to_json('ethereum','usd'))
 
-    # gck.write_original_to_json('ethereum','usd','max')
-    # print(gck.get_dates('ethereum'))
+    for crypto in tqdm.tqdm(cryptos):
+        print('\n',crypto,'\n')
 
-    # coin_list = Gecko().get_all_coins()
-    # print(coin_list)
-    # IDs = []
-    # for coin in coin_list:
-    #     IDs.append(coin['id'])
-    # print(IDs)
-    # BTC = Gecko().get_100d_history('bitcoin')
-    # print(BTC)
-    
-    # print(BTC.keys())
-    # BTC = Gecko().get_history('bitcoin')
-    # print(BTC)
-
-    # def get_100d_history_test(self,symbol,date):
-    #     dates = pd.date_range(end = dt.datetime.today(), periods = 100).to_pydatetime().tolist()
-    #     dates = [x.strftime('%d-%m-%Y') for x in dates]
-    #     res = pd.DataFrame()
-    #     # for date in dates:
-    #     all_data =  self.cg.get_coin_history_by_id(symbol,date)
-    #     market_data = all_data['market_data']['current_price']
-    #     print(list(market_data.keys()))
-    #     # res.loc[date] = 
-    
-    # def get_history(self,symbol,period):
-    #     """
-    #     takes symbol as lower full str name of crypto and period=days into past
-    #     """
-    #     dates = pd.date_range(end = dt.datetime.today(), periods = period).to_pydatetime().tolist()
-    #     dates = [x.strftime('%d-%m-%Y') for x in dates]
-    #     columns=['aed', 'ars', 'aud', 'bch', 'bdt', 'bhd', 'bmd', 'bnb', 'brl', 'btc', 'cad', 'chf', 'clp', 'cny', 'czk', 'dkk', 'dot', 'eos', 'eth', 'eur', 'gbp', 'hkd', 'huf', 'idr', 'ils', 'inr', 'jpy', 'krw', 'kwd', 'lkr', 'ltc', 'mmk', 'mxn', 'myr', 'ngn', 'nok', 'nzd', 'php', 'pkr', 'pln', 'rub', 'sar', 'sek', 'sgd', 'thb', 'try', 'twd', 'uah', 'usd', 'vef', 'vnd', 'xag', 'xau', 'xdr', 'xlm', 'xrp', 'yfi', 'zar', 'bits', 'link', 'sats']
-    #     res_dict = {}
-    #     for date in tqdm.tqdm(dates):
-    #         all_data =  self.cg.get_coin_history_by_id(symbol,date)
-    #         market_data = list([round(float(x),2) for x in all_data['market_data']['current_price'].values()])
-    #         res_dict[date] = market_data
-    #         time.sleep(0.75)
-    #     res = pd.DataFrame.from_dict(res_dict,orient='index',columns=columns)
-
-    #     return res
+        data = gck.write_to_json(crypto,'usd')
+        with open(f'{crypto}.json','w') as f:
+            json.dump(data.to_json(orient='split',date_format='iso'),f)        
+       
+    # xx = gck.write_to_json('ethereum','usd')
+    # with open('test_eth_1.json','w') as f:
+    #     json.dump(xx.to_json(orient='split',date_format='iso'),f)        
--- utils.py
+++ utils.py
@@ -2,10 +2,11 @@ import datetime as dt
 import pandas as pd
 
 def date_range_lister():
-    end = dt.datetime.today()
+    end = dt.date.today()
     diff = dt.timedelta(days=1827, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
     start = end - diff
     dates = pd.date_range(start=start,end=end,freq='D')
+    dates = [int(x.timestamp()) for x in dates]
     return list(dates)
 
 if __name__=="__main__":
