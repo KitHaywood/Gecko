9806fa64a26e1707ba0f82ebf348bea72e7cb7ef
Kit Haywood <kithaywood53@gmail.com>
2021-10-24 16:17:12 +0100

    unittest start

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+import unittest
+
+# Prelim Unit Testing
+
+class TestDataType(unittest.TestCase):
+    
+    def test_sysArgv(self):
+        self.assertIsInstance(sys.argv[1:],list)
+    
+    def test_sysArgv_type(self):
+        self.assertTrue(all([x for x in sys.argv[1:]]))
+        
+if __name__=="__main__":
+    unittest.main()
